58f786abed320b20554a7a925359320d7d5ba070
Saharsh Agrawal <saharshagrawal@berkeley.edu>
2019-10-20 23:22:17 -0700

    added code

--- pki.go
+++ pki.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
-
+	"crypto/rand"
+	"crypto/rsa"
+	"time"
+	"encoding/json"
 	"github.com/go-redis/redis"
 )
 
+var client = redis.NewClient(&redis.Options{
+	Addr:     "localhost:6379",
+	Password: "password",
+	DB:       0,
+})
+
 func main() {
 	fmt.Println("hi there!")
-
-	client := redis.NewClient(&redis.Options{
-		Addr:     "localhost:6379",
-		Password: "",
-		DB:       0,
-	})
 
 	pong, err := client.Ping().Result()
 	fmt.Println(pong, err)
@@ -22,10 +25,49 @@ func main() {
 	res, err := client.Get("name").Result()
 
 	fmt.Println(res)
+	fmt.Println(time.Now().AddDate(1,0,0))
+	// time.Now() = 2019-10-20 22:59:55.644117 -0700 PDT m=+1.734839423
+
+	issueIdentity("vvl")
+	res1, _ := client.Get("vvl").Result()
+	fmt.Println(res1)
+	// var c Certificate
+	// unmarshalled := json.Unmarshal([]byte(res1), c)
+	// fmt.Println(unmarshalled)
 }
 
-func issueIdentity() bool {
-	return false
+
+type Certificate struct {
+	PublicKey rsa.PublicKey
+	ILPAddress string
+	Expiration time.Time
+}
+
+func newCertificate(pub rsa.PublicKey, ILPAddress string) Certificate {
+	expiration := time.Now().AddDate(1,0,0)
+
+	c := Certificate{pub, ILPAddress, expiration}
+
+	// bytes, err := json.Marshal(c)
+	// fmt.Println(bytes)
+	// client.set(ILPAddress, )
+	return c
+}
+
+func issueIdentity(ILPAddress string) bool {
+
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+	pub := &priv.PublicKey
+
+	// fmt.Println(*pub)
+	c := newCertificate(*pub, ILPAddress)
+
+	bytes, _ := json.Marshal(c)
+	// fmt.Println(bytes)
+
+	client.Set(ILPAddress, bytes, 0)
+
+	return true
 }
 
 func reissueIdentity() bool {
@@ -37,6 +79,6 @@ func revokeIdentity() bool {
 }
 
 // should return a certificate
-func retrieveIdentity(ilpAddress string) {
-
+func retrieveIdentity(ilpAddress string) bool {
+	return false
 }
