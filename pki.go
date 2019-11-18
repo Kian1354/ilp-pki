06be12957ae79c4ef024ef9eb6c08663709eefe5
Kian Hooshmand <kianhooshmand@berkeley.edu>
2019-11-17 19:07:36 -0800

    cleaned up + documented IssueIdentity and RetrieveIdentity

--- pki.go
+++ pki.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"crypto"
 	"crypto/rand"
 	"crypto/rsa"
 	"encoding/json"
 	"fmt"
 	"time"
+	"reflect"
 
 	"github.com/go-redis/redis"
 )
 
+// Initializes a Redis client to store certificates.
 var client = redis.NewClient(&redis.Options{
 	Addr:     "localhost:6379",
 	Password: "",
@@ -18,32 +19,68 @@ var client = redis.NewClient(&redis.Options{
 })
 
 func main() {
-	pong, err := client.Ping().Result()
-	fmt.Print("Doing health check for redis client: ")
-	fmt.Println(pong, err)
+	pong, _ := client.Ping().Result()
+	fmt.Println("Doing health check for redis client: ", pong)
 
-	err = client.Set("name", "Kian", 0).Err()
-	res, err := client.Get("name").Result()
+	// issue an identity for ILP address "123"
+	privKey := IssueIdentity("123")
+	fmt.Println("Generated Private Key of type:", reflect.TypeOf(privKey))
 
-	fmt.Println(res)
-	fmt.Println(time.Now().AddDate(1, 0, 0))
-	// time.Now() = 2019-10-20 22:59:55.644117 -0700 PDT m=+1.734839423
+	// access public key elements
+	// privKey.PublicKey.N --> big modulus
+	// privKey.PublicKey.E --> public exponent
 
-	issueIdentity("vvl")
-	res1, _ := client.Get("vvl").Result()
-	fmt.Println(res1)
-	var c Certificate
-	_ = json.Unmarshal([]byte(res1), &c)
-	fmt.Println(c.ILPAddress)
+	// access private key elements
+	// privKey.D --> private exponent
+	// privKey.Primes --> prime factors of N
+
+
+	res := RetrieveIdentity("123")
+
+	fmt.Println("Retrieved certificate, with public key", res.PublicKey)
 }
 
+// Certificate struct contains the public key, ILP address, and the expiration time.
 type Certificate struct {
-	PublicKey  crypto.PublicKey
+	PublicKey  rsa.PublicKey
 	ILPAddress string
 	Expiration time.Time
 }
 
-func newCertificate(pub crypto.PublicKey, ILPAddress string) Certificate {
+// Given a ILP address, generate and store a new certificate; return the private key.
+func IssueIdentity(ILPAddress string) *rsa.PrivateKey {
+
+	// generate a 2048 bit RSA key
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+
+	// gets the public key portion as a *rsa.PublicKey object
+	// and create a certificate
+	pub := priv.PublicKey
+	c := newCertificate(pub, ILPAddress)
+
+	// marshal the certificate and store in the Redis client
+	bytes, _ := json.Marshal(c)
+	client.Set(ILPAddress, bytes, 0)
+
+	// return the private key
+	return priv
+}
+
+// Given an ILP address, retrieve the certificate from the client.
+func RetrieveIdentity(ilpAddress string) Certificate {
+
+	// get the marshaled bytes from the redis client
+	res, _ := client.Get(ilpAddress).Result()
+
+	// unmarshal the bytes into a certificate struct variable
+	var c Certificate
+	_ = json.Unmarshal([]byte(res), &c)
+
+	return c
+}
+
+// Given a public key and an ILP address, generate a new certificate.
+func newCertificate(pub rsa.PublicKey, ILPAddress string) Certificate {
 	expiration := time.Now().AddDate(1, 0, 0)
 
 	c := Certificate{pub, ILPAddress, expiration}
@@ -51,32 +88,12 @@ func newCertificate(pub crypto.PublicKey, ILPAddress string) Certificate {
 	return c
 }
 
-func issueIdentity(ILPAddress string) rsa.PrivateKey {
-
-	// generate a 2048 bit RSA key
-	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
-
-	// gets the public key portion as a crypto.PublicKey object
-	// and create a certificate
-	pub := priv.Public()
-	c := newCertificate(pub, ILPAddress)
-
-	// marshal the certificate and store
-	bytes, _ := json.Marshal(c)
-	client.Set(ILPAddress, bytes, 0)
-
-	return *priv
-}
-
+// TBD
 func reissueIdentity() bool {
 	return false
 }
 
+// TBD
 func revokeIdentity() bool {
-	return false
-}
-
-// should return a certificate
-func retrieveIdentity(ilpAddress string) Certificate {
 	return false
 }
