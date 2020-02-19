181384fdeefd537373120bcfb2b4120f1da2fe38
Saharsh Agrawal <saharshagrawal@berkeley.edu>
2020-02-18 22:32:38 -0800

    Refactored CA API

--- README.md
+++ README.md
@@ -1,8 +1,30 @@
 # ilp-pki
 
 Set up Redis 
-`docker pull redis`
+`docker pull redis`  
 `docker run --name redis-test-instance -p 6379:6379 -d redis`
 
 Get go-redis package
 `go get github.com/go-redis/redis`
+
+## Session Flow
+
+### Users
+- User1
+- User2
+
+### Steps
+1. User1 wishes to communicate with User2 after verifying User2’s identity.
+2. User2 offers User1 its ILP Address (and vic-versa).
+3. Using User1’s ILP Address, User2 uses the `RetrieveIdentity()` api call to retrieve the corresponding certificate.
+4. User2 now uses the `IsValidCertificate()` api call to verifiy that the retrieved certificate has not expired and that it is infact signed by the CA. If so, then User2 proceeds to communicate with User1.
+
+## Running Sample Program Flow
+Run `go run pki.go` in the `./cmd/main` directory
+
+## Running Unit Tests
+Run `go test` in the `./cmd/main` directory
+
+## Generate CA keys
+Perform this command first to set up CA credentials on CA server:  
+Run `go gen_ca.go` in the `./cmd/gen_ca` directory
---
+++ cmd/main/pki.go
@@ -0,0 +1,214 @@
+package main
+
+import (
+	"bufio"
+	"crypto"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+	"fmt"
+	"os"
+	"reflect"
+	"time"
+
+	"github.com/go-redis/redis"
+)
+
+var YEAR = 1
+var MONTH = 0
+var DAY = 0
+var PATH = "../gen_ca/ca_credentials/ca.key"
+
+// Initializes a Redis client to store certificates.
+var client = redis.NewClient(&redis.Options{
+	Addr:     "localhost:6379",
+	Password: "",
+	DB:       0,
+})
+
+func main() {
+	pong, _ := client.Ping().Result()
+	fmt.Println("Doing health check for redis client: ", pong)
+
+	// issue an identity for ILP address "123"
+	privKey, _ := IssueIdentity("567")
+	fmt.Println("Generated Private Key of type:", reflect.TypeOf(privKey))
+
+	// access public key elements
+	// privKey.PublicKey.N --> big modulus
+	// privKey.PublicKey.E --> public exponent
+
+	// access private key elements
+	// privKey.D --> private exponent
+	// privKey.Primes --> prime factors of N
+
+	// retrieve the corresponding identity for ILP address "123"
+	res, _ := RetrieveIdentity("567")
+	fmt.Println("Retrieved certificate, with public key", res.Cert.PublicKey)
+
+	// save the SignedCertificate to a file
+	SaveCertificateToFile(res)
+
+	// verify that the SignedCertificate is valid
+	valid := IsValidCertificate(res)
+	fmt.Println("Certificate is valid: ", valid)
+}
+
+type SignedCertificate struct {
+	Cert      Certificate
+	Signature []byte
+}
+
+// Certificate struct contains the public key, ILP address, and the expiration time.
+type Certificate struct {
+	PublicKey  rsa.PublicKey
+	ILPAddress string
+	Expiration time.Time
+}
+
+// Given a ILP address, generate and store a new certificate; return the private key.
+func IssueIdentity(ILPAddress string) (*rsa.PrivateKey, error) {
+	// generate a 2048 bit RSA key
+	priv, _ := rsa.GenerateKey(rand.Reader, 2048)
+
+	// gets the public key portion as a *rsa.PublicKey object
+	// and create a certificate
+	pub := priv.PublicKey
+	c := newCertificate(pub, ILPAddress)
+
+	signedC := newSignedCertificate(c)
+
+	// marshal the signed certificate and store in the Redis client
+	bytes, _ := json.Marshal(signedC)
+
+	expiration := c.Expiration
+	duration := time.Until(expiration)
+	client.Set(ILPAddress, bytes, duration)
+
+	// return the private key
+	return priv, nil
+}
+
+//RetrieveIdentity: given an ILP address, retrieve the certificate from the client.
+func RetrieveIdentity(ilpAddress string) (SignedCertificate, error) {
+
+	cert := &SignedCertificate{}
+	// get the marshaled bytes from the redis client
+	res, err := client.Get(ilpAddress).Result()
+
+	if err != nil {
+		return *cert, err
+	}
+
+	// unmarshal the bytes into a certificate struct variable
+	// var c Certificate
+	_ = json.Unmarshal([]byte(res), cert)
+
+	return *cert, nil
+}
+
+// Given an ILP address, remove the certificate from the DB.
+func revokeIdentity(ilpAddress string) error {
+	return client.Del(ilpAddress).Err()
+}
+
+// Given a certificate, check if it is valid; certificate is defined to be valid if signature is valid and certificate is not expired
+func IsValidCertificate(signedC SignedCertificate) bool {
+	return signedC.Cert.Expiration.After(time.Now()) && verifySignature(signedC.Cert, signedC.Signature)
+}
+
+// Saves a given certificate to a file.
+func SaveCertificateToFile(certificate SignedCertificate) (fileName string, err error) {
+	jsonData, err := json.Marshal(certificate)
+
+	// write to JSON file
+	jsonFile, err := os.Create("./certificate_files/certificate" + (certificate.Cert.ILPAddress)[0:3] + ".json")
+
+	if err != nil {
+		panic(err)
+	}
+	defer jsonFile.Close()
+
+	jsonFile.Write(jsonData)
+	jsonFile.Close()
+	fmt.Println("JSON data written to ", jsonFile.Name())
+
+	return jsonFile.Name(), err
+
+}
+
+// Given a public key and an ILP address, generate a new certificate.
+func newCertificate(pub rsa.PublicKey, ILPAddress string) Certificate {
+	expiration := time.Now().AddDate(YEAR, MONTH, DAY)
+	c := Certificate{pub, ILPAddress, expiration}
+
+	return c
+}
+
+func newSignedCertificate(cert Certificate) SignedCertificate {
+	bytes, _ := json.Marshal(cert)
+
+	sig := createSignature(bytes)
+
+	signedCert := SignedCertificate{cert, sig}
+
+	return signedCert
+}
+
+func createSignature(message []byte) []byte {
+	rng := rand.Reader
+
+	hash := sha256.Sum256(message)
+	MSK, _ := getCAKey()
+
+	sig, err := rsa.SignPKCS1v15(rng, MSK, crypto.SHA256, hash[:])
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error from signing: %s\n", err)
+		return nil
+	}
+
+	return sig
+}
+
+// Returns true if signature successfully verified
+func verifySignature(cert Certificate, signature []byte) bool {
+	bytes, _ := json.Marshal(cert)
+
+	hash := sha256.Sum256(bytes)
+
+	MSK, _ := getCAKey()
+
+	err := rsa.VerifyPKCS1v15(&MSK.PublicKey, crypto.SHA256, hash[:], signature)
+
+	return err == nil
+}
+
+// Server method for retrieving the CA private key from file.
+func getCAKey() (*rsa.PrivateKey, error) {
+
+	privKeyFile, _ := os.Open(PATH)
+
+	fileInfo, _ := privKeyFile.Stat()
+	var size = fileInfo.Size()
+
+	keyBytes := make([]byte, size)
+
+	buffer := bufio.NewReader(privKeyFile)
+	_, err := buffer.Read(keyBytes)
+
+	data, _ := pem.Decode([]byte(keyBytes))
+	privKeyFile.Close()
+
+	MSK, err := x509.ParsePKCS1PrivateKey(data.Bytes)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return MSK, nil
+}
